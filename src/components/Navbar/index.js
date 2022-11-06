e114294103864ebabe432060a19d5800fb1c698e
Ajanth Uthayakumaran <ajanth@Ajanths-MacBook-Air.local>
2022-11-06 01:53:22 -0500

    Add hacky fix to redirects when signing in

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react/cjs/react.production.min";
 import { Button, Heading, ListItem, UnorderedList } from "@chakra-ui/react";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 import { useRouter } from "next/router";
